518cc6bb23ad07e44f633fe92e28aab96ed899af
toXCustom <pawel89@gmail.com>
2023-01-30 12:42:40 +0100

    Initial commit

---
+++ src/test/java/pl/toxcustom/cafeManagementSystem/CafeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.toxcustom.cafeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
