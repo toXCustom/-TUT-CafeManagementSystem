984c88b3af03c3a1779685eceefdfed8921adc6a
toXCustom <pawel89@gmail.com>
2023-02-02 16:38:15 +0100

    Starting the Login API of User

---
+++ src/main/java/pl/toxcustom/cafeManagementSystem/JWT/CustomerUsersDetailsService.java
@@ -0,0 +1,38 @@
+package pl.toxcustom.cafeManagementSystem.JWT;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.toxcustom.cafeManagementSystem.dao.UserDao;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+@Slf4j
+@Service
+public class CustomerUsersDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserDao userDao;
+
+    private pl.toxcustom.cafeManagementSystem.POJO.User userDetail;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("Inside loadUserByUsername {}", username);
+        userDetail = userDao.findByEmailId(username);
+        if (!Objects.isNull(userDetail)) {
+            return new User(userDetail.getEmail(), userDetail.getPassword(), new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found!");
+        }
+    }
+
+    public pl.toxcustom.cafeManagementSystem.POJO.User getUserDetail() {
+        return userDetail;
+    }
+}
---
+++ src/main/java/pl/toxcustom/cafeManagementSystem/JWT/JwtFilter.java
@@ -0,0 +1,42 @@
+package pl.toxcustom.cafeManagementSystem.JWT;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private CustomerUsersDetailsService service;
+
+    Claims claims = null;
+    private String userName = null;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+
+        if (httpServletRequest.getServletPath().matches("/user/login|/user/forgetPassword|/user/signup")) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+        } else {
+            String authorizationHeader = httpServletRequest.getHeader("Authorization");
+            String token = null;
+
+            if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+                token = authorizationHeader.substring(7);
+                userName = jwtUtil.extractUsername(token);
+            }
+        }
+
+    }
+}
